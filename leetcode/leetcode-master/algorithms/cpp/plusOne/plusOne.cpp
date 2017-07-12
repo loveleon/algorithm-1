528e197c301ba785241d3c2f2f79894fb6b678fa
lizhiang <zhiangli029@gmail.com>
2017-07-13 01:52:57 +0800

    add plusOne

--- leetcode/leetcode-master/algorithms/cpp/plusOne/plusOne.cpp
+++ leetcode/leetcode-master/algorithms/cpp/plusOne/plusOne.cpp
@@ -12,8 +12,11 @@
 
 #include <iostream>
 #include <vector>
+#include <string.h>
+#include <stdlib.h>
 using namespace std;
 
+#if 0
 vector<int> plusOne(vector<int> &digits) {
     int carry=1;
     vector <int> v;
@@ -43,9 +46,72 @@ void printVector(vector<int>& v)
 
 int main()
 {
-    int a[]={9,9,9};
+    int a[]={9, 9, 9};
     vector<int> d(&a[0], &a[0]+sizeof(a)/sizeof(int));
     vector<int> v = plusOne(d);
     printVector(v);
     return 0;
 }
+
+#else
+
+
+void printVector(int* v, int len)
+{
+    cout << "{ ";
+    for(int i = 0; i< len; i++){
+        cout << v[i] << " ";
+    }
+    cout << "} " << endl;
+}
+
+int* plusOne(int* digits, int digitsSize, int* returnSize)
+{
+	int i = 0;
+	int carry = 1;
+	int sum = 0;
+	int rec = 0;
+
+	for (i = digitsSize - 1; i >= 0; i--)
+	{
+		sum = digits[i] + carry;
+		digits[i] = sum % 10;
+		carry = sum / 10;
+	}
+
+	if (carry > 0)
+	{
+		//int new_arr[digitsSize + 1];
+		int *new_arr = (int *)malloc(sizeof(int) * (digitsSize + 1));
+		if (!new_arr)
+		{
+			return NULL;
+		}
+		memset(new_arr, 0, digitsSize + 1);
+		new_arr[0] = 1;
+		memcpy(&new_arr[1], digits, digitsSize);
+		*returnSize = digitsSize + 1;
+		//printVector(digits, digitsSize);
+		return new_arr;
+	}
+	else
+	{
+		*returnSize = digitsSize;
+		return digits;	
+	}
+}
+
+
+int main()
+{
+    int a[]={9, 9, 9};
+    
+	int new_l = 0;
+	int *new_d = NULL;
+	
+    new_d = plusOne(a, (sizeof(a)/sizeof(a[0])), &new_l);
+    printVector(new_d, new_l);
+    return 0;
+}
+
+#endif
