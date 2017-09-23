cf8d6b508b6de341e0b6ed7e2710279f884e586b
lizhiang <zhiangli029@gmail.com>
2017-09-23 16:03:15 +0800

    add LowestCommonAncestorOfABinarySearchTree in leetcode

--- leetcode/leetcode-master/algorithms/cpp/lowestCommonAncestorOfABinarySearchTree/LowestCommonAncestorOfABinarySearchTree.cpp
+++ leetcode/leetcode-master/algorithms/cpp/lowestCommonAncestorOfABinarySearchTree/LowestCommonAncestorOfABinarySearchTree.cpp
@@ -36,6 +36,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+#if 0
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -55,3 +57,31 @@ public:
         return NULL;
     }
 };
+
+#else 
+
+class Solution {
+public:
+  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+  {
+    while (root)
+    {
+      if ((p->val > root->val) && (q->val > root->val))
+      {
+        root = root->right;
+      }
+      else if ((p->val < root->val) && (q->val < root->val))
+      {
+        root = root->left;
+      }
+      else
+      {
+        return root;
+      }
+    }
+
+    return NULL;
+  }
+};
+
+#endif
