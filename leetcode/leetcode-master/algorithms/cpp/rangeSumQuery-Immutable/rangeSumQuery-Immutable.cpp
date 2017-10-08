449a1194d366be44c951b1095e19fb707a5cbc06
lizhiang <zhiangli029@gmail.com>
2017-10-09 00:36:29 +0800

    add rangeSumQuery-Immutable

--- leetcode/leetcode-master/algorithms/cpp/rangeSumQuery-Immutable/rangeSumQuery-Immutable.cpp
+++ leetcode/leetcode-master/algorithms/cpp/rangeSumQuery-Immutable/rangeSumQuery-Immutable.cpp
@@ -45,6 +45,35 @@ public:
     }
 };
 
+class NumArray01 {
+/* 
+ * Solution
+ * =========
+ *
+ * The sum of all the elements starting from position 0 to position i is stored in
+ * sums[i]. This way we can reconstruct the sum from position i to position j by
+ * subtracting sums[i - 1] from sums[j], leaving us with the sum of the desired elements.
+ *
+ * Note: we can add a dummy sum at then beginning to simplify the code
+ * 
+ */
+private:
+  int size;
+  vector <long long> sums;
+public:
+  NumArray(vector<int> &nums): size(nums.size()), sums(size + 1, 0) {
+    for (int i = 0; i < size; i++)
+    {
+      sums[i + 1] = sums[i] + nums[i];
+    }
+  }
+
+  int sumRange(int i, int j) {
+     return sums[j + 1] - sums[i];
+  }
+};
+
+
 
 // Your NumArray object will be instantiated and called as such:
 // NumArray numArray(nums);
